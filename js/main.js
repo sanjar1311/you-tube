ab505c0ba7610d807d441a963b5549d7e49d6e0c
sanjar1311 <sanjarabduxamidov1311@gmail.com>
2021-01-03 15:40:23 +0500

    add localStorage function

--- js/main.js
+++ js/main.js
@@ -67,47 +67,56 @@ elMenuBtn.addEventListener('click', function() {
   elMain.classList.toggle('active');
 });
 
-/* ========== Videos Output ========== */
-
-elGussie.addEventListener('click', function() {
-
+var outputFunc = function(arrey) {
   elUserPic.classList.add('site-header__gussie');
   elFirstLineUser.textContent = 'Gussie Singleton';
   $_('.first-line__inner').classList.add('first-line__inner--active');
 
   var firstLineTemplate = $_('.first-line-template').content;
   var videoFragment = document.createDocumentFragment();
+
   elFisrstLineList.innerHTML = '';
-  for(var i = 0; i < videos.length; i++) {
+  for(var i = 0; i < arrey.length; i++) {
     var firstLineItem = firstLineTemplate.cloneNode(true);
     var videoTitle = $_('.video__title', firstLineTemplate);
-    videoTitle.textContent = videos[i].title;
+    videoTitle.textContent = arrey[i].title;
     videoTitle.setAttribute('class', 'video__title');
     var videoViews = $_('.video__views', firstLineTemplate);
-    videoViews.textContent = videos[i].views;
+    videoViews.textContent = arrey[i].views;
     videoViews.setAttribute('class', 'video__views');
     var videoTime = $_('.video__time', firstLineTemplate);
-    videoTime.textContent = videos[i].day;
+    videoTime.textContent = arrey[i].day;
     videoTime.setAttribute('class', 'video__time');
     var videoAuthor = $_('.video__author', firstLineTemplate);
-    videoAuthor.textContent = videos[i].author;
+    videoAuthor.textContent = arrey[i].author;
     videoAuthor.setAttribute('class', 'video__author');
     var videoDuration = $_('.video__duration', firstLineTemplate);
-    videoDuration.textContent = videos[i].duration;
+    videoDuration.textContent = arrey[i].duration;
     videoDuration.setAttribute('class', 'video__duration');
     var videoImg = $_('.video__img', firstLineTemplate);
-    videoImg.setAttribute('src', videos[i].img);
+    videoImg.setAttribute('src', arrey[i].img);
     var videoAddBtn = $_('.video__add', firstLineItem);
     videoAddBtn.dataset.id = i;
-    console.log(videoAddBtn.dataset.id);
 
      videoFragment.appendChild(firstLineItem);
-
   }
   elFisrstLineList.appendChild(videoFragment);
-  console.log(elFisrstLineList)
+}
 
+/* ========== Videos Output ========== */
+
+var videosL = JSON.parse(localStorage.getItem('arrey'));
+if(videosL) {
+  outputFunc(videos);
+}
+
+elGussie.addEventListener('click', function() {
+
+  outputFunc(videos);
+
+  localStorage.setItem('arrey', JSON.stringify(videos));
 });
+
 
 /* ======== Add Favourite ========= */
 elFisrstLineList.addEventListener('click', function(evt) {
@@ -118,8 +127,9 @@ elFisrstLineList.addEventListener('click', function(evt) {
 
   if(evt.target.matches('.video__add')){
     for(var i = 0; i < videos.length; i++) {
-      if(videos[i].id == evt.target.dataset.id){
-        favourite.push(videos[i]);
+      if(videos[i].id === Number(evt.target.dataset.id)){
+        var index = Number(evt.target.dataset.id);
+        favourite.push(videos[index]);
       }
     }
   }
